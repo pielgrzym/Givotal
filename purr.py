ab21d9993e536dcdd1032526a616d6fbbd323c7b
Jakub Nawalaniec <pielgrzym@prymityw.pl>
2012-06-23 12:53:05 +0200

    Implemented automated token fetching

--- purr.py
+++ purr.py
@@ -2,14 +2,31 @@
 # encoding: utf-8
 
 import subprocess
+import getpass
+import urllib
+import urllib2
+from xml.dom import minidom
+
+
+def getToken():
+    print "Pivotal token not found in your gitconfig."
+    sysuser = getpass.getuser()
+    username = raw_input("Username [%s]:" % sysuser)
+    username = username or sysuser
+    password = getpass.getpass()
+    data = urllib.urlencode({'username': username, 'password': password})
+    request = urllib2.Request("https://www.pivotaltracker.com/services/v3/tokens/active", data)
+    try:
+        response = urllib2.urlopen(request)
+    except urllib2.HTTPError:
+        print "Wrong username or password"
+        exit(1)
+    dom = minidom.parseString(response.read())
+    return dom.getElementsByTagName('guid')[0].firstChild.data
 
 try:
-    API_KEY = subprocess.check_output(['git', 'config', 'givotal.apikey'])
+    API_KEY = subprocess.check_output(['git', 'config', 'givotal.token'])
 except subprocess.CalledProcessError:
-    print """Pivotal api key not found in your gitconfig.
-    Please add it via:
-        >>> git config --global givotal.apikey = "<YOUR_API_KEY>"
-    """
-    exit(1)
+    API_KEY = subprocess.check_output(['git', 'config', 'givotal.token', getToken()])
 
 print API_KEY
