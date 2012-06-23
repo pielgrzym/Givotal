a227094ebde48db74270c454e81889c2fa849f68
Jakub Nawalaniec <pielgrzym@prymityw.pl>
2012-06-23 17:56:56 +0200

    Backlog directory structure creation

--- purr.py
+++ purr.py
@@ -5,12 +5,17 @@ import os
 import codecs
 import shutil
 import subprocess
+import argparse
 from util.pivotal import Pivotal
 
-p = Pivotal()
+parser = argparse.ArgumentParser(prog="purr",
+        description="Pivotal Update Remote Registry tool")
+parser.add_argument('-c', '--current', action='store_true', help="Fetch stories from current")
+parser.add_argument('-b', '--backlog', action='store_true', help="Fetch stories from backlog")
 
 
 def mk_current():
+    print "Fetching current.."
     basedir = "current"
     iterations = p.get("https://www.pivotaltracker.com/services/v3/projects/%s/iterations/current" % p.PROJECT_ID)
     current_dirs = []
@@ -21,7 +26,6 @@ def mk_current():
         except OSError:
             pass
         current_dirs.append(story['id'][0])
-        print story['id'][0]
         with codecs.open(os.path.join(path, 'story'), 'w', 'utf-8') as storyfile:
             storyfile.write(u"Name: %s \n" % story['name'][0])
             storyfile.write(u"Type: %s \n" % story['story_type'][0])
@@ -34,4 +38,41 @@ def mk_current():
             with open("/dev/null", 'w') as null:
                 subprocess.call(['git', 'rm', '-rf', os.path.join(basedir, story)], stderr=null)
 
-mk_current()
+
+def mk_backlog():
+    print "Fetching backlog.."
+    basedir = "backlog"
+    iterations = p.get("https://www.pivotaltracker.com/services/v3/projects/%s/iterations/backlog" % p.PROJECT_ID)
+    current_dirs = []
+    for it, iteration in enumerate(iterations['iteration']):
+        iteration = iteration['number'][0]
+        for i, story in enumerate(iterations['iteration'][it]['stories'][0]['story']):
+            path = os.path.join(basedir, iteration, story['id'][0])
+            try:
+                os.makedirs(path)
+            except OSError:
+                pass
+            current_dirs.append(story['id'][0])
+            with codecs.open(os.path.join(path, 'story'), 'w', 'utf-8') as storyfile:
+                storyfile.write(u"Name: %s \n" % story['name'][0])
+                storyfile.write(u"Type: %s \n" % story['story_type'][0])
+                storyfile.write(u"Current state: %s \n" % story['current_state'][0])
+                storyfile.write(u"Description:\n%s \n" % story['description'][0])
+        # now time to unlink stale stories
+    for it, iteration in enumerate(iterations['iteration']):
+        iteration = iteration['number'][0]
+        for story in os.listdir(os.path.join(basedir, iteration)):
+            if story not in current_dirs:
+                shutil.rmtree(os.path.join(basedir, iteration, story))
+                with open("/dev/null", 'w') as null:
+                    subprocess.call(['git', 'rm', '-rf', os.path.join(basedir, iteration, story)], stderr=null)
+
+
+if __name__ == "__main__":
+
+    p = Pivotal()
+    args = parser.parse_args()
+    if args.current:
+        mk_current()
+    if args.backlog:
+        mk_backlog()
