8478553d2abf0a5c45eef1b3ebc57d32405d2f0d
Jakub Nawalaniec <pielgrzym@prymityw.pl>
2012-06-29 15:50:51 +0200

    Brushed up branch creation and added very simple current show

--- git-pv.sh
+++ git-pv.sh
@@ -7,25 +7,28 @@ test -z "$1" && usage
 ACTION="$1"; shift
 
 test -z "$GIVOTAL_REF" && GIVOTAL_REF="$(git config givotal.ref)"
-test -z "$GIVOTAL_REF" && GIVOTAL_REF="refs/heads/pivotal-db"
+test -z "$GIVOTAL_REF" && GIVOTAL_REF="refs/heads/pivotal/master"
 
 case "$ACTION" in
 fetch)
         PREV_REF="$(git symbolic-ref HEAD 2>/dev/null)"
         PREV_REF=${PREV_REF##refs/heads/}
-        if [ -z $(git show-ref --verify --quiet $GIVOTAL_REF) ]; then
-                say "Creating Givotal orphaned branch..."
-                git checkout --orphan $GIVOTAL_REF
-                git rm -rf .
+        if $(git show-ref --quiet $GIVOTAL_REF); then
+                git checkout $GIVOTAL_REF &>/dev/null
         else
-                git checkout $GIVOTAL_REF
+                say "Creating Givotal orphaned branch..."
+                git checkout --orphan $GIVOTAL_REF &>/dev/null
+                git rm -rf . &>/dev/null
         fi
         purr.py --current
         git add current
         purr.py --backlog
         git add backlog
-        git commit -m "Msg"
+        git commit -m "Fetchted pivotal data" &>/dev/null
         git checkout $PREV_REF
+;;
+current)
+        git grep "Name:" refs/heads/pivotal/master:current | sed 's/^[^[:blank:]]*//;s/^[ \t0-9\.]*//' | sort -h
 ;;
 *)
 	usage
