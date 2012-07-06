c86fde90fe924bf76ed6cbd4d9e3e24a0c4e7010
Jakub Nawalaniec <pielgrzym@prymityw.pl>
2012-07-06 22:44:20 +0200

    Found the real problem with review errors

--- git-pv.sh
+++ git-pv.sh
@@ -160,8 +160,9 @@ review | rv)
                 fi
                 git fetch "$REMOTE"
                 BRANCH=$(git branch -r | grep "$REMOTE/$PARAM1-")
+                BRANCH="${BRANCH##*[[:blank:]]}"
                 LBRANCH=${BRANCH##$REMOTE/} # remote/1234-my -> 1234-my
-                if $(git show-ref --quiet $LBRANCH); then
+                if $(git show-ref --quiet "$LBRANCH"); then
                         echo "Local branch $LBRANCH exists"
                         echo "Merge remote (default) or replace? [m/r] "
                         read MR
@@ -169,18 +170,18 @@ review | rv)
                                 [rR] )
                                         # replace branch in case there was a forced update
                                         git checkout "$(git config givotal.integration-branch)"
-                                        git branch -D "${LBRANCH// /}"
-                                        git checkout -t "${BRANCH// /}"
+                                        git branch -D "$LBRANCH"
+                                        git checkout -t "$BRANCH"
                                         ;;
                                 *)
                                         # just add fixes after redelivery
-                                        git checkout "${LBRANCH// /}"
-                                        git merge "${BRANCH// /}"
+                                        git checkout "$LBRANCH"
+                                        git merge "$BRANCH"
                                         ;;
                         esac
                 else
                         # create new tracking branch to see the work
-                        git checkout -t "${BRANCH// /}"
+                        git checkout -t "$BRANCH"
                 fi
         fi
         ;;
